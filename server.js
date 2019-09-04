8e56f0830bfe05f8c25c56b4cc1f1349d414cde5
Sean Connolly <seanconnolly@ufl.edu>
2019-09-04 13:17:44 -0400

    Third commit

--- server.js
+++ server.js
@@ -8,19 +8,20 @@ var listingData, server;
 
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
-  if (parsedUrl.pathname == '/listings' | "/listings/") {
-response.writeHead(200, {'Content-Type': 'application/json'});
-response.write(listingData);
-response.end();
-  } else {
-response.writeHead(404);
-response.write('Not found');
-response.end('Bad gateway error');
-  }
+  if (parsedUrl.pathname == '/listings') {
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.write(listingData);
+    response.end();
+    } 
+  else {
+    response.writeHead(404);
+    response.end('Bad gateway error');
+  } 
+  
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  if (err) console.log(err);
+ // if (err) console.log(err);
   listingData = data;
   server = http.createServer(requestHandler);
   server.listen(port, function() {
